e7d5dfbb68bd51421879084e6ec0cac0250ed2d0
DungNg <87799163+nminhdung@users.noreply.github.com>
2023-12-19 11:23:41 +0700

    middleware error

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,24 +1,25 @@
 import User from "../models/user.js";
 import bcryptjs from 'bcryptjs'
+import { errorHandler } from "../middlewares/error.js";
 
-const signUp = async (req, res) => {
+const signUp = async (req, res, next) => {
     console.log(req.body)
     const { username, email, password } = req.body;
     if (!username || !email || !password) throw new Error('Missing input');
-    const hashedPassword = bcryptjs.hashSync(password, 10)
-    const newUser = await User.create({ username, email, password: hashedPassword })
+    const hashedPassword = bcryptjs.hashSync(password, 10);
+    const newUser = await User.create({ username, email, password: hashedPassword });
     try {
         if (newUser) {
             res.status(201).json({
                 result: newUser,
-                mes:'User created successfully' 
+                mes: 'User created successfully'
             })
         }
     } catch (error) {
-        res.status(500).json(error.message)
+        next(error);
     }
 
-    
+
 }
 export const authController = {
     signUp
