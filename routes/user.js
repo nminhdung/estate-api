05d1c1d63f61939cbec185938e761faf8dd0b3a4
DungNg <87799163+nminhdung@users.noreply.github.com>
2023-12-25 12:13:20 +0700

    delete user

--- routes/user.js
+++ routes/user.js
@@ -7,5 +7,6 @@ import { verifyToken } from "../middlewares/verifyUser.js";
 const router = express.Router();
 router.get('/test', userController.test);
 router.put('/update/:id', verifyToken, userController.updateUser);
+router.delete('/delete/:id', verifyToken, userController.deleteUser);
 
 export const userRouter = router;
